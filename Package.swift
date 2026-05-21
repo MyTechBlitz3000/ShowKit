5bcea505f0f8a128e697ce331c972014b4162e4d
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-21 18:59:13 +0800

    Add initial Package.swift for ShowKit library

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// Package.swift
+
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "ShowKit",
+
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+
+    products: [
+        .library(
+            name: "ShowKit",
+            targets: ["ShowKit"]
+        )
+    ],
+
+    targets: [
+        .target(
+            name: "ShowKit"
+        ),
+
+        .testTarget(
+            name: "ShowKitTests",
+            dependencies: ["ShowKit"]
+        )
+    ]
+)
