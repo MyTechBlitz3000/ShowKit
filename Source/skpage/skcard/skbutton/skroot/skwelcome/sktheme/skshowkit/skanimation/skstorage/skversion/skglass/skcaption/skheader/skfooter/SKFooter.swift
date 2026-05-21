47a864a40514cfef2d5527df828a2e3445340f40
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-21 19:09:46 +0800

    Add SKFooter view to display footer text

---
+++ Source/skpage/skcard/skbutton/skroot/skwelcome/sktheme/skshowkit/skanimation/skstorage/skversion/skglass/skcaption/skheader/skfooter/SKFooter.swift
@@ -0,0 +1,15 @@
+// skfooter/SKFooter.swift
+
+import SwiftUI
+
+public struct SKFooter: View {
+
+    public init() {}
+
+    public var body: some View {
+
+        Text("Powered by ShowKit")
+            .font(.footnote)
+            .foregroundStyle(.tertiary)
+    }
+}
