f6fd8ce3ec5282c7d06161223b9ccad89a5ff5b2
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-21 19:06:49 +0800

    Add SKAnimation enum for animation types

---
+++ Source/skpage/skcard/skbutton/skroot/skwelcome/sktheme/skshowkit/skanimation/SKAnimation.swift
@@ -0,0 +1,12 @@
+// skanimation/SKAnimation.swift
+
+import SwiftUI
+
+public enum SKAnimation {
+
+    public static let defaultAnimation: Animation = .smooth
+
+    public static let bounce: Animation = .bouncy
+
+    public static let spring: Animation = .spring
+}
