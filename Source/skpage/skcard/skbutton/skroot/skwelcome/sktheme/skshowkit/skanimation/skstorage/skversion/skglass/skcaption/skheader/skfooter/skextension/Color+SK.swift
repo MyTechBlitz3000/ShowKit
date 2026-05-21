53d6bd36ae42c5fb18a1450aa24abc9855658a1f
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-21 19:11:05 +0800

    Add Color+SK.swift for custom Color extensions

---
+++ Source/skpage/skcard/skbutton/skroot/skwelcome/sktheme/skshowkit/skanimation/skstorage/skversion/skglass/skcaption/skheader/skfooter/skextension/Color+SK.swift
@@ -0,0 +1,14 @@
+// skextensions/Color+SK.swift
+
+import SwiftUI
+
+public extension Color {
+
+    static let skBackground = Color(
+        uiColor: .systemBackground
+    )
+
+    static let skSecondaryBackground = Color(
+        uiColor: .secondarySystemBackground
+    )
+}
